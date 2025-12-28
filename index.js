d2c89ebf39405269cd27594194f5c1b5d1fe2936
shankarpattanaik <shankarpattanaik2000@gmail.com>
2025-12-28 09:41:48 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "shankar_pattanaik",
+    content:
+      "Learning to code is not about memorizing syntax. It’s about learning how to think and solve problems step by step.",
+  },
+  {
+    id: uuidv4(),
+    username: "rahul_kumar",
+    content:
+      "Failure is not the opposite of success. It is a part of success that teaches you what books never can.",
+  },
+  {
+    id: uuidv4(),
+    username: "ajay_kumar",
+    content:
+      "Consistency beats motivation. Even small progress every day can lead to massive results over time.",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => p.id === id);
+  res.render("edit.ejs", { post });
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => p.id === id);
+  res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => p.id === id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => p.id !== id);
+  res.redirect("/posts");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+  console.log("http://localhost:3000/posts");
+});
